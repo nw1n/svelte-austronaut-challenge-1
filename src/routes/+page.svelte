4d674df6013f825cae97a14868e6adfe1648509e
Yannik Inniger <yannik.inniger@ti8m.ch>
2024-03-13 15:04:52 +0100

    Implement welcome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<h1>Welcome to the Austronaut Challenge!</h1>
+
+	<span>🌎🚀🪐</span>
+
+	<p>
+		Send these guys to space <a href="http://api.open-notify.org/astros.json">
+			http://api.open-notify.org/astros.json
+		</a>
+	</p>
+</main>
+
+<style>
+	main {
+		font-family: 'Roboto';
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	span {
+		font-size: 48px;
+		letter-spacing: 8px;
+	}
+</style>
