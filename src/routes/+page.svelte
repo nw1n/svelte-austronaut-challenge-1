8f8179f01226d2a10a4f022386ade298cdd5019d
Yannik Inniger <yannik.inniger@ti8m.ch>
2024-03-13 15:52:52 +0100

    Update entry page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <main>
 	<h1>Welcome to the Austronaut Challenge!</h1>
 
-	<span>🌎🚀🪐</span>
-
+	<h2>Todays goal</h2>
 	<p>
 		Send these guys to space <a href="http://api.open-notify.org/astros.json">
 			http://api.open-notify.org/astros.json
 		</a>
 	</p>
+	<img src="/goal.png" alt="goal" />
 </main>
 
 <style>
