79e75924d04f7af8c38e2e89306401e56c8924e4
Yannik Inniger <yannik.inniger@ti8m.ch>
2024-03-13 15:51:15 +0100

    Move space stations to new page

--- src/routes/space-stations/+page.svelte
+++ src/routes/space-stations/+page.svelte
@@ -1,23 +1,22 @@
 <main>
-	<h1>Welcome to the Austronaut Challenge!</h1>
-
-	<span>🌎🚀🪐</span>
-
-	<p>
-		Send these guys to space <a href="http://api.open-notify.org/astros.json">
-			http://api.open-notify.org/astros.json
-		</a>
-	</p>
+	<pre>insert space stations</pre>
 </main>
 
 <style>
 	main {
 		font-family: 'Roboto';
+		color: white;
 
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
+
+		background-image: url('/background.jpg');
+		background-size: cover;
+
+		height: 100vh;
+		width: 100vw;
 	}
 
 	span {
